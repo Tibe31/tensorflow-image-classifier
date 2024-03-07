1629d2eb150ba63869fd3b14db05f00c72893f8a
Matteo Baldelli <62099945+baldellimtt@users.noreply.github.com>
2024-03-07 17:28:37 +0100

    Update augmentation_cfg.py

--- ImageClassification/augmentation_cfg.py
+++ ImageClassification/augmentation_cfg.py
@@ -9,11 +9,5 @@ train_augmentation_parameters = {
 }
 
 val_augmentation_parameters = {
-    "rescale": 1./255.,
-    "rotation_range": 2,
-    "zoom_range": 0.02,
-    "width_shift_range": 0.02,
-    "height_shift_range": 0.02,
-    "brightness_range": [0.9, 1.1],
-    "fill_mode": "nearest"
+    "rescale": 1./255.
 }
