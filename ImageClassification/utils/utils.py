0a9f2e21de69b8cbaf5cf19036cc96ba42c6d566
Matteo Baldelli <62099945+baldellimtt@users.noreply.github.com>
2024-03-08 16:02:25 +0100

    update

--- ImageClassification/utils/utils.py
+++ ImageClassification/utils/utils.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
-def show_augmentations(BATCH_SIZE,x):
+
+#show all the images in the training batch
+def show_augmentations(BATCH_SIZE,train_generator):
   x= train_generator.next()
   for i in range(0,BATCH_SIZE-1):
     image = x[0][i]
